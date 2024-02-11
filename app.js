69d853f63f254376a1a3afa8a4219597a842d54a
Talhaayub <khantalharahi@gmail.com>
2024-02-11 23:03:38 +0500

    add some changes in controller and app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const connectDB = require('./db/connect');
 
 const mainRouter = require('./routes/main');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -24,7 +25,7 @@ app.use(errorHandlerMiddleware);
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
--- controllers/main.js
+++ controllers/main.js
@@ -1,26 +1,39 @@
-const CustomAPIError = require('../errors/custom-error');
 
+
+const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
 
 
 
 const login = async (req, res) => {
      const {username, password} = req.body;
 
+     const id = new Date().getDate();
+
+
      if(!username || !password){
           throw new CustomAPIError("Please provide a username and password", 400);
      }
 
+     const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
+     console.log(token);
+     res.status(200).json({msg: "user created successfully!!", token});
 
-     // console.log(username, password);
-
-     res.send("Login Successfully!");
+     // res.send("Login Successfully!");
 }
 
 const dashboard = async (req, res) => {
+     const authHeader = req.headers.authorization;
+
+     console.log(authHeader);
+
+     if(!authHeader || !authHeader.startsWith('Bearer ')) {
+          throw new CustomAPIError(" No Token Provided", 401); 
+     }
+
      const luckNum = Math.floor(Math.random() * 100);
      res.status(200).send({msg: `Hello john`, secret: `here is your lucky num ${luckNum}`});
 }
-
 
 
 module.exports = {
